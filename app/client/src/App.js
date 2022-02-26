9798d273ebbe3b33f3c39c5eedb309f67c814f4a
sb4255 <57270552+sb4255@users.noreply.github.com>
2022-02-26 12:02:23 -0500

    Finshed Buttons

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -6,11 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div className="Log-In"><Button variant="Log-In" >Log In</Button></div>
+       <div className="Sign-In"><Button variant="Sign-in">Create Account</Button></div>
         <img src={logo} className="App-logo" alt="logo" />
         <div className ="title-text">WELCOME TO MOTION !!! </div>
         <div className="content-text">The app that will help you stay organized and keep track of your tasks</div>
-        <Button variant="Log-In" >Log In</Button>
-        <Button className="Sign-in">Create Account</Button>
+        <div className="Boards"><Button variant="boards">Go To Your Boards</Button></div>
       </header>
     </div>
   );
