13795b99405716e44e9c7acb729bfa824492ba4e
Andy Chek <achek2019@gmail.com>
2022-03-02 11:12:04 -0500

    new scheduler API

--- app/client/src/components/Calendar.jsx
+++ app/client/src/components/Calendar.jsx
@@ -6,6 +6,9 @@ import React from "react";
 this is the calendar API I used:
 https://ej2.syncfusion.com/react/documentation/api/calendar/#calendarcomponent
 
+scheduleComponent API:
+https://ej2.syncfusion.com/react/documentation/api/schedule#properties
+
 using this tutorial:
 https://www.youtube.com/watch?v=wgqX295fGkY
 */
