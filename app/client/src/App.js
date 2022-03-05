ba19787d023d26ce4e5360186ad0891f497405b6
jessicaxjiang <jess.jiang22@gmail.com>
2022-03-05 13:12:30 -0500

    pls fix the homepage this time

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -19,18 +19,21 @@ const Home = () => (
 
 function App() {
   return (
-    <div className="App">
-        <div className="Log-In"><Button variant="Log-In" >Log In</Button></div>
+    <div>
+      <div className="App">
+        <div className="Log-In"><Button variant="Log-In">Log In</Button></div>
         <div className="Sign-In"><Button variant="Sign-in">Create Account</Button></div>
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/todo" element={<Todo />} />
-            <Route path="/calendar" element={<Calendar />} />
-          </Routes>
-        </div>
-      </Router>
+      </div><div>
+        <Router>
+          <div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/todo" element={<Todo />} />
+              <Route path="/calendar" element={<Calendar />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
     </div>
   );
 }
