f58257e75bc9cd0b9dfb43d9e191a79aaaa98342
spender101 <spencer.fan@yahoo.com>
2022-03-07 14:06:42 -0500

    Added functions for event calls

--- app/client/src/Calendar.jsx
+++ app/client/src/Calendar.jsx
@@ -27,4 +27,115 @@ function Calendar() {
   );
 }
 
+//addevent
+function addevent() {
+  // let eventtitle = body.eventtitle;
+  // let eventdate = body.eventdate;
+  // let eventdescription = body.eventdescription;
+  // let eventstarttime = body.eventstarttime;
+  // let eventendtime = body.eventendtime;
+  // let eventlocation = body.eventlocation;
+  // let eventisrepetition = body.eventrepetition;
+
+  console.log(eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdescription, eventisrepetition);
+  let data = {
+    'eventtitle': eventtitle, 'eventdate': eventdate, 'eventstarttime': eventstarttime, 'eventendtime': eventendtime, 'eventlocation': eventlocation, 'eventdescription': eventdescription, 'eventisrepetition': eventisrepetition
+  };
+
+  fetch('/addevent', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  }).then(function (response) {
+    console.log(response.status); // will be 400 if request failed
+    if (response.status === 200) {
+      msg.textContent = "Success"
+    } else {
+      msg.textContent = "Bad request"
+    }
+  }).catch(function (error) {
+    console.log(error); // in case fetch crashes for some reason
+  });
+}
+
+//updateevent
+function updateevent() {
+  // let eventtitle = body.eventtitle;
+  // let eventdate = body.eventdate;
+  // let eventdescription = body.eventdescription;
+  // let eventstarttime = body.eventstarttime;
+  // let eventendtime = body.eventendtime;
+  // let eventlocation = body.eventlocation;
+  // let eventisrepetition = body.eventrepetition;
+
+  console.log(eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdescription, eventisrepetition);
+  let data = {
+    'eventtitle': eventtitle, 'eventdate': eventdate, 'eventstarttime': eventstarttime, 'eventendtime': eventendtime, 'eventlocation': eventlocation, 'eventdescription': eventdescription, 'eventisrepetition': eventisrepetition
+  };
+
+  fetch('/updateevent', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  }).then(function (response) {
+    console.log(response.status); // will be 400 if request failed
+    if (response.status === 200) {
+      msg.textContent = "Success"
+    } else {
+      msg.textContent = "Bad request"
+    }
+  }).catch(function (error) {
+    console.log(error); // in case fetch crashes for some reason
+  });
+}
+
+//returnallevents
+function returnAllevents() {
+  let url = `/returnallevents`;
+  fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    console.log("Client received from server:", data);
+  }).catch(function (error) {
+    console.log(error); // in case fetch crashes for some reason
+  });
+}
+
+//returnevent
+function returnevent() {
+  // let eventtitle = body.eventtitle;
+
+  let url = `/returnevent?title=${eventtitle}`;
+  fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    console.log("Client received from server:", data);
+  }).catch(function (error) {
+    console.log(error); // in case fetch crashes for some reason
+  });
+}
+
+//deletevent
+function deletevent() {
+  // let eventtitle = body.eventtitle;
+
+  console.log(eventtitle);
+  let data = { 'eventtitle': eventtitle };
+
+  fetch('/deletevent', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  }).then(function (response) {
+    console.log(response.status); // will be 400 if request failed
+    if (response.status === 200) {
+      msg.textContent = "Success"
+    } else {
+      msg.textContent = "Bad request"
+    }
+  }).catch(function (error) {
+    console.log(error); // in case fetch crashes for some reason
+  });
+}
+
 export default Calendar;
