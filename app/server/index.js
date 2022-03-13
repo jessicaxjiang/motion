6619d915fc59855aae77b9b5f565b9afa1196127
jessicaxjiang <jess.jiang22@gmail.com>
2022-03-12 19:11:47 -0500

    now this will fix it

--- app/server/index.js
+++ app/server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const cors = require("cors");
 
 const corsOptions = {
-   origin:'http://localhost:3001/', 
+   origin:'http://localhost:3000', 
    credentials:true,
    optionSuccessStatus:200,
 }
