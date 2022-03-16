97d2ef6cb6e90a17231ff74a92d8f6b194198d31
Andy Chek <achek2019@gmail.com>
2022-03-16 14:39:01 -0400

    form can insert to table, async issue with fetching

--- app/client/src/components/Calendar.jsx
+++ app/client/src/components/Calendar.jsx
@@ -13,7 +13,14 @@ https://ej2.syncfusion.com/react/documentation/api/schedule#properties
 using this tutorial:
 https://www.youtube.com/watch?v=wgqX295fGkY
 */
-let data= [{
+
+
+
+
+
+
+
+let data = [{
   Id: 1,
   EndTime: new Date(2022, 4, 1, 6, 30),
   StartTime: new Date(2022, 4, 1, 6, 0),
@@ -23,13 +30,30 @@ let data= [{
 }]
 
 function Calendar() {
+
+  let serverData = {}
+  let url = `http://localhost:3001/returnallevents`;
+  fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    console.log("Client received from server:", data);
+    serverData = data.dataSource
+    for (let event of serverData) {
+      event.startTime = new Date(event.startTime);
+      event.endTime = new Date(event.endTime);
+    }
+    console.log("changing to date objects: ", serverData);
+  }).catch(function (error) {
+    console.log(error); // in case fetch crashes for some reason
+  });
+
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="Calendar">
         Calendar
       </div>
-      <ScheduleComponent eventSettings={{ dataSource: data}}>
+      <ScheduleComponent eventSettings={{ dataSource: serverData }}>
         <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
       </ScheduleComponent>
     </div>
